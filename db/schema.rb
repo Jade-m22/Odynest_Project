c3d651e9059a88a62c012e38b5680a3bc7ea5c80
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-19 10:34:44 +0100

    experience_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_03_19_091709) do
+  create_table "experiences", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "providers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
