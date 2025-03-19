c9b5581616ad4fa8c0b7f9cda8e5d5f024bd6103
Jade-m22 <mj.micheljade@gmail.com>
2025-03-19 11:08:09 +0100

    add admin dashboard to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,60 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: %i[show edit update destroy dashboard]
+  before_action :authorize_admin!, only: %i[destroy]
   
-    # Affiche le profil utilisateur
-    def show
-      @reservations = @user.reservations.includes(:experience)
-    end
+  def new
+    @‌user = User.new
+  end
   
-    # Formulaire d'édition du profil
-    def edit
-    end
-  
-    # Mise à jour du profil
-    def update
-      if @user.update(user_params)
-        redirect_to @user, notice: "Profil mis à jour avec succès !"
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-  
-    # Suppression du compte
-    def destroy
-      @user.destroy
-      redirect_to root_path, notice: "Votre compte a bien été supprimé."
-    end
-  
-    private
-  
-    # Récupérer l'utilisateur en fonction de l'ID
-    def set_user
-      @user = User.find(params[:id])
-    end
-  
-    # Paramètres autorisés pour la mise à jour du profil
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :avatar)
+  def create
+    @‌user = User.new(user_params)
+    if @‌user.save
+      redirect_to root_path, notice: 'Compte utilisateur créé avec succès.'
+    else
+      render :new
     end
   end
   
+  def show
+  end
+  
+  def dashboard
+    if current_user.admin?
+      @‌experiences = Experience.all
+      @‌reservations = Reservation.all
+    else
+      @‌reservations_current = current_user.reservations.where("reservation_date >= ?", Time.now)
+      @‌reservations_past = current_user.reservations.where("reservation_date < ?", Time.now)
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @‌user.update(user_params)
+      redirect_to dashboard_path, notice: 'Profil mis à jour.'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @‌user.destroy
+    redirect_to root_path, notice: 'Compte supprimé.'
+  end
+  
+  private
+
+  def set_user
+    @‌user = current_user
+  end
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :admin)
+  end
+  
+  def authorize_admin!
+    redirect_to root_path unless current_user.admin?
+    end
+  end
