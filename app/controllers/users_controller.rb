12009b349d0dd2b23b90b8d0326da99694221982
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-19 12:04:38 +0100

    fixcontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy dashboard]
   before_action :authorize_admin!, only: %i[destroy]
+  before_action :authenticate_user! 
+
+  helper_method :user_signed_in?, :current_user
   
   def new
     @‌user = User.new
