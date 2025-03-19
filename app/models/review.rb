fd3fe9de053ab56eddcb95e8f2e692e0be64feae
Jade-m22 <mj.micheljade@gmail.com>
2025-03-19 13:40:21 +0100

    add presence true to comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :experience
 
   validates :rating, presence: true, inclusion: { in: 1..5 }
-  validates :comment
+  validates :comment, presence: true
 end
