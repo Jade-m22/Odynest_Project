d9e300f789268d47a764ff549c9f47b218e4e320
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-19 11:32:57 +0100

    show experiences

--- app/controllers/experience_controller.rb
+++ app/controllers/experience_controller.rb
@@ -1,57 +1,60 @@
 class ExperiencesController < ApplicationController
     before_action :set_experience, only: %i[show edit update destroy]
     before_action :authorize_provider_or_admin!, only: %i[new create edit update destroy]
-    
+  
     def index
-    @‌experiences = Experience.all
+      @experiences = Experience.all
     end
-    
+  
     def show
+        @experience = Experience.find(params[:id])
     end
-    
+  
     def new
-    @‌experience = current_provider.experiences.build
+      @experience = current_provider.experiences.build
     end
-    
+  
     def create
-    @‌experience = current_provider.experiences.build(experience_params)
-    if @‌experience.save
-    redirect_to @‌experience, notice: 'Expérience créée avec succès.'
-    else
-    render :new
+      @experience = current_provider.experiences.build(experience_params)
+      if @experience.save
+        redirect_to @experience, notice: 'Expérience créée avec succès.'
+      else
+        render :new
+      end
     end
-    end
-    
+  
     def edit
     end
-    
+  
     def update
-    if @‌experience.update(experience_params)
-    redirect_to @‌experience, notice: 'Expérience mise à jour avec succès.'
-    else
-    render :edit
+      if @experience.update(experience_params)
+        redirect_to @experience, notice: 'Expérience mise à jour avec succès.'
+      else
+        render :edit
+      end
     end
-    end
-    
+  
     def destroy
-    if @‌experience.reservations.empty?
-    @‌experience.destroy
-    redirect_to experiences_path, notice: 'Expérience supprimée.'
-    else
-    redirect_to @‌experience, alert: 'Impossible de supprimer une expérience avec des réservations.'
+      if @experience.reservations.empty?
+        @experience.destroy
+        redirect_to experiences_path, notice: 'Expérience supprimée.'
+      else
+        redirect_to @experience, alert: 'Impossible de supprimer une expérience avec des réservations.'
+      end
     end
-    end
-    
+  
     private
+  
     def set_experience
-    @‌experience = Experience.find(params[:id])
+      @experience = Experience.find(params[:id])
     end
-    
+  
     def experience_params
-    params.require(:experience).permit(:title, :description, :price, :location, :duration, :provider_id)
+      params.require(:experience).permit(:title, :description, :price, :location, :duration, :provider_id)
     end
-    
+  
     def authorize_provider_or_admin!
-    redirect_to root_path unless current_admin? || current_provider
+      redirect_to root_path unless current_admin? || current_provider
     end
-    end
+  end
+  
