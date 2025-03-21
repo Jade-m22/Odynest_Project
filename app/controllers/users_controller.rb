fac8dc8b02885e594c6a39054b7a9cbf2272ef6a
Fonnard Jeremy <jeremfrd@gmail.com>
2025-03-21 13:31:59 +0400

    admin modify profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    def show
-      render "users/sessions/show"
-    end
-    
+    render "users/sessions/show"
   end
 
   def new
@@ -21,15 +18,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: "Compte utilisateur cr\u00E9\u00E9 avec succ\u00E8s."
+      redirect_to root_path, notice: "Compte utilisateur créé avec succès."
     else
       render :new
     end
   end
 
   def dashboard
-    @reservations = current_user.reservations
-    @experiences = Experience.joins(:reservations).where(reservations: { user_id: current_user.id })
+    if current_user.is_admin?
+      @experiences = Experience.includes(:users)
+      @users = User.all
+      @providers = Provider.all
+    else
+      @reservations = current_user.reservations.includes(:experience)
+      @experiences = @reservations.map(&:experience)
+    end
   end
 
   def edit
@@ -37,7 +40,13 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to user_dashboard_path, notice: "Profil mis \u00E0 jour."
+      if current_user.admin?
+        # Si c'est un admin, on redirige vers le profil de l'utilisateur
+        redirect_to user_path(@user), notice: "Profil mis à jour."
+      else
+        # Si ce n'est pas un admin, on redirige vers le dashboard de l'utilisateur
+        redirect_to user_dashboard_path, notice: "Profil mis à jour."
+      end
     else
       render :edit
     end
@@ -45,7 +54,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to root_path, notice: "Compte supprim\u00E9."
+    redirect_to root_path, notice: "Compte supprimé."
   end
 
   private
@@ -60,6 +69,7 @@ class UsersController < ApplicationController
   end
 
   def authorize_user
+    # L'admin doit pouvoir modifier son propre profil
     unless current_user.admin? || current_user == @user
       redirect_to root_path, alert: "Accès interdit."
     end
