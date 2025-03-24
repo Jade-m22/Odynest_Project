cb4fc0671e98942b6c61f7b07970efb6fd28ee49
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-24 18:11:45 +0100

    secured password suggestion

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,28 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
+
+function generatePassword() {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  let password = "";
+  for (let i = 0; i < 16; i++) {
+    password += charset[Math.floor(Math.random() * charset.length)];
+  }
+
+  document.getElementById(
+    "suggested-password"
+  ).innerText = `Mot de passe suggéré : ${password}`;
+
+  // Remplir le champ automatiquement
+  const passwordField = document.getElementById("user_password");
+  if (passwordField) {
+    passwordField.value = password;
+  }
+}
