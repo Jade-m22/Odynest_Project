b433a588b6e652703c3889a82130311804bce409
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-25 08:28:57 +0100

    js cgu warning

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,3 +20,19 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("registration-form");
+  const checkbox = document.getElementById("accept_terms");
+
+  if (form && checkbox) {
+    form.addEventListener("submit", (event) => {
+      if (!checkbox.checked) {
+        event.preventDefault();
+        alert(
+          "Veuillez accepter les conditions générales d'utilisation pour continuer."
+        );
+      }
+    });
+  }
+});
