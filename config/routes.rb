bbca449f8e0a126d9c495334a04d0b353b4ee84f
GaetanBordes <bordes.gaetan33@gmail.com>
2025-03-25 18:29:28 +0100

    add opendys option

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "users/dashboard", to: "users#dashboard", as: "user_dashboard"
   get "user/settings", to: "users#settings", as: :user_settings
   get "provider/settings", to: "providers#settings", as: :provider_settings
+  get "/toggle_font", to: "fonts#toggle", as: :toggle_font
 
   resources :users, only: %i[index show edit update destroy]
   resources :providers, only: %i[index edit update destroy]
