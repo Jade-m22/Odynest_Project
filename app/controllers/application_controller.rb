03ceddab535aa9a6da10f473de590c91cf47c4e1
Jade-m22 <mj.micheljade@gmail.com>
2025-03-26 10:31:03 +0100

    add permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # Permet l'ajout des paramètres nécessaires pour User et Provider
+  def configure_permitted_parameters
+    # Pour l'inscription et la mise à jour de l'utilisateur
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username ])
+
+    # Pour l'inscription et la mise à jour du provider
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :first_name ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name ])
+  end
 end
