921d3f6a03d4b554c6cd93870f4182c06e9fd367
Jade-m22 <mj.micheljade@gmail.com>
2025-03-28 21:55:16 +0100

    change host for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :letter_opener
 
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: '87.106.231.213', port: 3000 }
 
 
   config.action_mailer.delivery_method = :smtp
